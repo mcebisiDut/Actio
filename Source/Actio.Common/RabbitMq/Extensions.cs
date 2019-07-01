b76a9a7fea6da4cc72138e597d316620aa275322
mcebisim <mcebisim@sahomeloans.com>
2019-07-01 16:10:13 +0200

    API configurations

--- Source/Actio.Common/RabbitMq/Extensions.cs
+++ Source/Actio.Common/RabbitMq/Extensions.cs
@@ -2,7 +2,10 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Actio.Common.ICommands;
 using Actio.Common.IEvents;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using RawRabbit;
+using RawRabbit.Instantiation;
 using RawRabbit.Pipe;
 
 namespace Actio.Common.RabbitMq
@@ -24,5 +27,17 @@ namespace Actio.Common.RabbitMq
 
         private static string GetQueueName<T>()
             => $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}";
+
+        public static void AddRabbitMq(this IServiceCollection service, IConfiguration configuration)
+        {
+            var options = new RabbitMqOptions();
+            var section = configuration.GetSection("rabbitmq");
+            section.Bind(options);
+            var client = RawRabbitFactory.CreateSingleton(new RawRabbitOptions
+            {
+                ClientConfiguration = options
+            });
+            service.AddSingleton<IBusClient>(_ => client);
+        }
     }
 }
