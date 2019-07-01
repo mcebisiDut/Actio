4caa7f8d8947a7466f651657b517cc59cc96d093
Paul Cluver <paulcl@sahomeloans.com>
2019-07-01 10:41:23 +0200

    Class for user authentication event

---
+++ Source/Actio.Common/Events/UserAuthenticated.cs
@@ -0,0 +1,16 @@
+using Actio.Common.IEvents;
+
+namespace Actio.Common.Events
+{
+    public class UserAuthenticated : IEvent
+    {
+        public string Email { get; }
+        protected UserAuthenticated()
+        {
+        }
+        public UserAuthenticated(string email)
+        {
+            Email = email;
+        }
+    }
+}
